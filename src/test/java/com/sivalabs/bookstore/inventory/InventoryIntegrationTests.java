8fc432774f32a69f90dbdc58f2c826b332991e6a
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-09-03 10:28:07 +0530

    Fix test

--- src/test/java/com/sivalabs/bookstore/inventory/InventoryIntegrationTests.java
+++ src/test/java/com/sivalabs/bookstore/inventory/InventoryIntegrationTests.java
@@ -3,16 +3,19 @@ package com.sivalabs.bookstore.inventory;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+import com.sivalabs.bookstore.TestcontainersConfiguration;
 import com.sivalabs.bookstore.inventory.internal.InventoryService;
 import com.sivalabs.bookstore.orders.domain.events.OrderCreatedEvent;
 import java.time.Duration;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Import;
 import org.springframework.modulith.test.ApplicationModuleTest;
 import org.springframework.modulith.test.Scenario;
 
 @ApplicationModuleTest(webEnvironment = RANDOM_PORT)
+@Import(TestcontainersConfiguration.class)
 class InventoryIntegrationTests {
     @Autowired
     private InventoryService inventoryService;
