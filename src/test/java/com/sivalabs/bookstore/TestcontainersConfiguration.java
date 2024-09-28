c8e558ed7d5220f09dc3301744954bb9a9402e93
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-09-28 08:53:03 +0530

    Upgrade postgres version to 17

--- src/test/java/com/sivalabs/bookstore/TestcontainersConfiguration.java
+++ src/test/java/com/sivalabs/bookstore/TestcontainersConfiguration.java
@@ -14,7 +14,7 @@ public class TestcontainersConfiguration {
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"))
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:17-alpine"))
                 .withDatabaseName("postgres")
                 .withUsername("postgres")
                 .withPassword("postgres");
