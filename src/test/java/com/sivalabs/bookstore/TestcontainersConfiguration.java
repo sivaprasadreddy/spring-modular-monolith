b0251f6d56be603acb404b47437ed0322c1c818e
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-11-15 18:52:06 +0530

    Using Testcontainers singleton pattern

--- src/test/java/com/sivalabs/bookstore/TestcontainersConfiguration.java
+++ src/test/java/com/sivalabs/bookstore/TestcontainersConfiguration.java
@@ -6,29 +6,39 @@ import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
+@Testcontainers
 public class TestcontainersConfiguration {
+
+    @Container
+    static PostgreSQLContainer postgres = new PostgreSQLContainer<>(DockerImageName.parse("postgres:17-alpine"));
+
+    @Container
+    static RabbitMQContainer rabbitmq = new RabbitMQContainer(DockerImageName.parse("rabbitmq:4.0.2-alpine"));
+
+    @Container
+    static GenericContainer<?> zipkin =
+            new GenericContainer<>(DockerImageName.parse("openzipkin/zipkin:3.4.2")).withExposedPorts(9411);
 
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:17-alpine"))
-                .withDatabaseName("postgres")
-                .withUsername("postgres")
-                .withPassword("postgres");
+        return postgres;
     }
 
     @Bean
     @ServiceConnection
     RabbitMQContainer rabbitmq() {
-        return new RabbitMQContainer(DockerImageName.parse("rabbitmq:4.0.2-alpine"));
+        return rabbitmq;
     }
 
     @Bean
     @ServiceConnection(name = "openzipkin/zipkin")
     GenericContainer<?> zipkinContainer() {
-        return new GenericContainer<>(DockerImageName.parse("openzipkin/zipkin:3.4.2")).withExposedPorts(9411);
+        return zipkin;
     }
 }
