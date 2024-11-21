da59cd0f2f7dd3134072fb47e79aa935a3e80575
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-11-21 13:41:06 +0530

    Updated inventory module to store data in DB

--- src/test/java/com/sivalabs/bookstore/inventory/InventoryIntegrationTests.java
+++ src/test/java/com/sivalabs/bookstore/inventory/InventoryIntegrationTests.java
@@ -8,6 +8,7 @@ import com.sivalabs.bookstore.orders.domain.events.OrderCreatedEvent;
 import com.sivalabs.bookstore.orders.domain.models.Customer;
 import java.time.Duration;
 import java.util.UUID;
+import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.modulith.test.ApplicationModuleTest;
@@ -22,10 +23,19 @@ class InventoryIntegrationTests {
     @Test
     void handleOrderCreatedEvent(Scenario scenario) {
         var customer = new Customer("Siva", "siva@gmail.com", "9987654");
-        var event = new OrderCreatedEvent(UUID.randomUUID().toString(), "P114", 2, customer);
-        scenario.publish(event)
-                .andWaitAtMost(Duration.ofSeconds(15))
-                .andWaitForStateChange(() -> inventoryService.getStockLevel("P114"))
-                .andVerify(stockLevel -> assertThat(stockLevel).isEqualTo(598));
+        String productCode = "P114";
+        var event = new OrderCreatedEvent(UUID.randomUUID().toString(), productCode, 2, customer);
+        /*
+                scenario.publish(event)
+                        .andWaitForStateChange(() -> inventoryService.getStockLevel("P114"))
+                        .andVerify(stockLevel -> assertThat(stockLevel).isEqualTo(598));
+        */
+        scenario.publish(event).andWaitForStateChange(() -> productCode).andVerify(code -> Awaitility.await()
+                .atMost(Duration.ofSeconds(10))
+                .pollInterval(Duration.ofSeconds(2))
+                .untilAsserted(() -> {
+                    Long stockLevel = inventoryService.getStockLevel(code);
+                    assertThat(stockLevel).isEqualTo(598);
+                }));
     }
 }
