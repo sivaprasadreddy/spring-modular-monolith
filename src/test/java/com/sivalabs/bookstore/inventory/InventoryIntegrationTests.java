2f41a695fcbed8c7eddc5dc4e98efa87ee0ca3ac
K. Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-11-21 18:15:24 +0530

    Simplify scenario test

--- src/test/java/com/sivalabs/bookstore/inventory/InventoryIntegrationTests.java
+++ src/test/java/com/sivalabs/bookstore/inventory/InventoryIntegrationTests.java
@@ -1,14 +1,11 @@
 package com.sivalabs.bookstore.inventory;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 import com.sivalabs.bookstore.TestcontainersConfiguration;
 import com.sivalabs.bookstore.orders.domain.events.OrderCreatedEvent;
 import com.sivalabs.bookstore.orders.domain.models.Customer;
-import java.time.Duration;
 import java.util.UUID;
-import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.modulith.test.ApplicationModuleTest;
@@ -25,17 +22,6 @@ class InventoryIntegrationTests {
         var customer = new Customer("Siva", "siva@gmail.com", "9987654");
         String productCode = "P114";
         var event = new OrderCreatedEvent(UUID.randomUUID().toString(), productCode, 2, customer);
-        /*
-                scenario.publish(event)
-                        .andWaitForStateChange(() -> inventoryService.getStockLevel("P114"))
-                        .andVerify(stockLevel -> assertThat(stockLevel).isEqualTo(598));
-        */
-        scenario.publish(event).andWaitForStateChange(() -> productCode).andVerify(code -> Awaitility.await()
-                .atMost(Duration.ofSeconds(10))
-                .pollInterval(Duration.ofSeconds(2))
-                .untilAsserted(() -> {
-                    Long stockLevel = inventoryService.getStockLevel(code);
-                    assertThat(stockLevel).isEqualTo(598);
-                }));
+        scenario.publish(event).andWaitForStateChange(() -> inventoryService.getStockLevel(productCode) == 598);
     }
 }
