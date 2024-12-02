341121eed80797ab4a18b8aebb3519b9ce1031d6
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2024-12-02 11:24:12 +0530

    Upgrade versions

--- src/main/java/com/sivalabs/bookstore/catalog/web/ProductWebController.java
+++ src/main/java/com/sivalabs/bookstore/catalog/web/ProductWebController.java
@@ -3,7 +3,7 @@ package com.sivalabs.bookstore.catalog.web;
 import com.sivalabs.bookstore.catalog.Product;
 import com.sivalabs.bookstore.catalog.ProductService;
 import com.sivalabs.bookstore.common.models.PagedResult;
-import io.github.wimdeblauwe.htmx.spring.boot.mvc.HxRequest;
+import io.github.wimdeblauwe.htmx.spring.boot.mvc.HtmxRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -27,19 +27,13 @@ class ProductWebController {
     }
 
     @GetMapping("/products")
-    String showProducts(@RequestParam(defaultValue = "1") int page, Model model) {
+    String showProducts(@RequestParam(defaultValue = "1") int page, Model model, HtmxRequest hxRequest) {
         log.info("Fetching products for page: {}", page);
         PagedResult<Product> productsPage = productService.getProducts(page);
         model.addAttribute("productsPage", productsPage);
+        if (hxRequest.isHtmxRequest()) {
+            return "partials/products";
+        }
         return "products";
-    }
-
-    @HxRequest
-    @GetMapping("/partials/products")
-    String getProductsPartial(@RequestParam(defaultValue = "1") int page, Model model) {
-        log.info("Fetching products-partial for page: {}", page);
-        PagedResult<Product> productsPage = productService.getProducts(page);
-        model.addAttribute("productsPage", productsPage);
-        return "partials/products";
     }
 }
